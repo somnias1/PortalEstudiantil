b8dcc1d26dd54474effbc3d95a780ff23e075882
Lizardo <lizardo@utp.edu.co>
2021-09-21 13:13:07 -0500

    Cambios en campos requeridos

--- api/models/estudiantes.py
+++ api/models/estudiantes.py
@@ -6,16 +6,16 @@ from django.contrib.auth.models import AbstractUser
 class Estudiante(AbstractUser):
     cedula = models.IntegerField('Cédula del estudiante', unique=True)
     USERNAME_FIELD = 'cedula'
-    nombre_1 = models.CharField('Primer nombre', max_length=128)
-    nombre_2 = models.CharField('Primer nombre', max_length=128)
-    Apellido_1 = models.CharField('Primer nombre', max_length=128)
-    apellido_2 = models.CharField('Primer nombre', max_length=128)
+    REQUIRED_FIELDS = ['username']
+    nombre_1 = models.CharField('Primer nombre', max_length=128, blank=True, null= True)
+    nombre_2 = models.CharField('Primer nombre', max_length=128, blank=True, null= True)
+    Apellido_1 = models.CharField('Primer nombre', max_length=128, blank=True, null= True)
+    apellido_2 = models.CharField('Primer nombre', max_length=128, blank=True, null= True)
     fecha_nacimiento = models.DateField(
         "Fecha de nacimiento", 
-        help_text= "YYYY-MM--DD", 
-        null=False)
-    residencia = models.CharField('Dirección de residencia', max_length=255)
-    
+        help_text= "YYYY-MM--DD", blank=True, null= True)
+    residencia = models.CharField('Dirección de residencia', max_length=255, blank=True, null= True)
+
 
     class Meta:
         verbose_name = "Estudiante"
